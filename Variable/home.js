eeb809b03bcfc95fcfb933f9392f7be097dfc8f1
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-05-31 20:22:28 +0530

    Variable in js

---
+++ Variable/home.js
@@ -0,0 +1,20 @@
+
+//Variable
+
+var b = 'Damindu';
+console.log(b);
+
+var age = 23;
+console.log(age);
+
+document.getElementById('title').innerHTML='Welcome';
+document.getElementById('name').innerHTML='Hey There';
+
+var name = prompt('What is your name ?');
+document.getElementById('firstname').innerHTML=name;
+
+
+
+
+
+ 
