be0292fb36518999d9024fb9d17a62018efcfe7b
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-05-31 22:43:10 +0530

    Numbers

---
+++ Numbers/home.js
@@ -0,0 +1,24 @@
+
+//Numbers
+var num1 = 5;
+var num2 = 5.8;
+
+console.log(5*10);
+
+console.log(50/10);
+
+console.log(5+10);
+
+console.log(5-10);
+
+num1 =  num1+5;
+
+console.log(num1++);
+
+console.log(num1--);
+
+console.log(50%4);
+
+console.log(num1+=10);
+
+console.log(num1-=5);
