2f2c5bcc7a4c8a6ff49e6edf31c32ea89cd518df
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-06-01 13:02:21 +0530

    Datatypes and string function

---
+++ Datatypes/home.js
@@ -0,0 +1,29 @@
+
+
+//Data types
+
+let yourAge = 23;
+let yourName = 'Damindu';
+let name = {first:'Damindu', last:'Sandaruwan'}; //object
+let boole = false;
+let food = ['apple','rice','koththu','mango']; //array
+let random; //undefined
+let nothing = null;
+
+//Strings
+let fruit = 'banana';
+let moreFruit = 'banana\napple';
+console.log(moreFruit);
+
+console.log(fruit.length);
+console.log(fruit.indexOf('an'));
+console.log(fruit.slice(2,6));
+
+console.log(fruit.replace('ban','123'));
+console.log(fruit.toUpperCase());
+console.log(fruit.toLowerCase());
+
+console.log(fruit.charAt(2));
+console.log(fruit[2]);
+
+console.log(fruit.split(''));
