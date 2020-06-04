2fc2bd768697f88bde12be14444a92285b8fe268
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-06-04 17:29:58 +0530

    Backend function done

--- CHALLENGE/js/script.js
+++ CHALLENGE/js/script.js
@@ -27,3 +27,51 @@ function generateDog(){
 }
 
 //CHALLENGE 3
+function rpsGame(yourChoice){
+    console.log(yourChoice);
+    console.log(yourChoice.src);
+
+    var humanChoice,botChoice;
+    humanChoice = yourChoice.id;
+    botChoice = numberToChoice(randToRpsInt());
+    console.log('BOT CHOICE: '+botChoice);
+    
+    results =decideWinner(humanChoice,botChoice); //[0,1] human lost | bot won
+    console.log('Scores: ' +results);
+    
+    message = finalMessage(results); //{'message':'You won','color':'green'}
+    console.log(message);
+    //rpsFrontEnd(yourChoice.id,botChoice,message);
+
+}
+
+function randToRpsInt(){
+    return Math.floor(Math.random()*3);
+}
+
+function numberToChoice(number){
+    return ['rock','paper','scissor'][number];
+}
+
+function decideWinner(yourChoice,computerChoice){
+    var rpsDatabase = {
+        'rock': {'scissor':1, 'rock':0.5, 'paper':0},
+        'paper':{'rock':1,'paper':0.5,'scissor':0},
+        'scissor':{'paper':1,'scissor':0.5,'rock':0},
+    }
+
+    var yourScore = rpsDatabase[yourChoice][computerChoice];
+    var computerScore = rpsDatabase[computerChoice][yourChoice];
+
+    return [yourScore,computerScore];
+}
+
+function finalMessage([yourScore,computerScore]){
+    if(yourScore==0){
+        return {'message':'You lost!','color':'red'};
+    }else if(yourScore==0.5){
+        return {'message':'You tied!','color':'yellow'};
+    }else{
+        return {'message':'You Won!','color':'green'};
+    }
+}
