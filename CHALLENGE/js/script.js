c3e1d349c1ac4061203549b674729824f1425dd7
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-06-06 22:20:29 +0530

    Challenege 5 add image

--- CHALLENGE/js/script.js
+++ CHALLENGE/js/script.js
@@ -158,3 +158,21 @@ function buttonRandom(){
 }
 
 //CHALLENGE 5
+let blackjackGame={
+    'you': {'scoreSpan':'#your-blackjack-result','div':'#your-box','score':0},
+    'dealer': {'scoreSpan':'#your-blackjack-result','div':'#dealer-box','score':0},
+};
+
+const YOU = blackjackGame['you'];
+const DEALER = blackjackGame['dealer'];
+
+document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackhit);
+
+function blackjackhit(){
+    // alert('hit clicked');
+    let cardImage = document.createElement('img');
+    cardImage.src= 'images/Q.png';
+    document.querySelector(YOU['div']).appendChild(cardImage);
+
+}
+
